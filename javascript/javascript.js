ba762b59639b8abca76aad598d113dc351fc1efa
IamElaya <asison13@gmail.com>
2016-02-09 02:22:55 +0000

    added jQuery

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,38 +1,55 @@
 
-number = Math.round((Math.random() * 10))%10 + 1;
+ // var guess = window.guess.value;
+  // var bet = window.bet.value;
 
+  // if(bet != null) {
+  //   bet = 3;
+  // }
+  // window.bet.value
 
-initial_money = 100;
+// do
+//     {
+//       // guess = prompt("What is your guess?");
+//       // var bet = parseInt(prompt("How much do you want to bet?"));
+     
 
-do
-    {
-      guess = prompt("What is your guess?");
-      var bet = parseInt(prompt("How much do you want to bet?"));
-      
-      if (guess == number) {
+//     } while (initial_money > 0);
+
+// // console.log(number);
+// console.log(initial_money);
+
+// var bet;
+var initial_money = 100;
+ 
+$('form').on('submit', function(e){
+  e.preventDefault(); 
+ 
+  var number = Math.round((Math.random() * 10))%10 + 1;
+  
+  var guess = $('#guess').val();
+  var bet = $('#bet').val();
+  
+ if (guess == number) {
         initial_money += bet;
-        document.write("Correct!");
-        document.write("You now have #"+initial_money+"dollars");
+        $("h2").text("Correct!");
+        $("span").text("You now have #"+initial_money+"dollars");
       }
       else {
         if (guess == (number + 1) || guess == (number - 1)) {
-          document.write("Close! You don't lose anymoney");
-          document.write("You now have #"+initial_money+"dollars");
+          $("h2").text("Close! You don't lose anymoney");
+          $("span").text("You now have #"+initial_money+"dollars");
         }
         else {
           (initial_money) -= (bet);
-          document.write("Wrong guess!");
-          document.write("You now have #"+initial_money+"dollars");
+          $("h2").text("Wrong guess!");
+          $("span").text("You now have #"+initial_money+"dollars");
         }
-      }
+      };
+  console.log(bet, guess);
+});
 
 
 
-    } while (initial_money > 0);
-
-console.log(number);
-console.log(initial_money);
- 
 
 
 
